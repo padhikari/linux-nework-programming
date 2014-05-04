42b2b4f8f2604de1c0a5680733c251b6c02c4e3d
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-05-03 22:51:09 -0700

    server using multiplexing

---
+++ sockets/src/multiple_client/server1.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+    
+    struct sockaddr_in sAddr;
+    fd_set readset,testset;
+    int listensock;
+    int newsock;
+    char buffer[25];
+    int result;
+    int nread;
+    int x;
+    int val;
+    
+    //create the listening socket
+    listensock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    
+    val = 1;
+    result = setsockopt(listensock, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+    if( result < 0){
+        perror("server1");
+        return 0;
+    }
+    
+    //bind the socket to the listening port
+    sAddr.sin_family = AF_INET;
+    sAddr.sin_port = htons(1972);
+    sAddr.sin_addr.s_addr = INADDR_ANY;
+    
+    result = bind(listensock, (struct sockaddr *)&sAddr, sizeof(sAddr));
+    if(result < 0){
+        perror("server1");
+        return 0;
+    }
+    
+    //initialize descriptor set using FD_ZERO and add the listening socket to the set
+    FD_ZERO(&readset);
+    FD_SET(listensock, &readset);
+    
+    while (1) {
+        testset = readset;
+        result = select(FD_SETSIZE, &testset, NULL, NULL, NULL);
+        if (result < 1) {
+            perror("server1");
+            return 0; }
+        for (x = 0; x < FD_SETSIZE; x++) {
+            if (FD_ISSET(x, &testset)) {
+                //if the activity is on the listening socket, accept the newconnection and add its socket to our watch set, otherwise, read characters from the client, if character read is less than or equal zero, then the client is assumed to have closed the connection, remove the descriptor from watch list, otherwise echo the charaters to the screen
+                if (x == listensock) {
+                    newsock = accept(listensock, NULL,NULL);
+                    FD_SET(newsock, &readset);
+                } else {
+                    nread = recv(x, buffer, 25, 0);
+                    if (nread <= 0) {
+                        close(x);
+                        FD_CLR(x, &readset);
+                        printf("client on descriptor #%i disconnected\n", x);
+                    } else {
+                        buffer[nread] = '\0';
+                        printf("%s\n", buffer);
+                        send(x, buffer, nread, 0);
+                    }
+                }
+            }
+        }
+    }
+}
